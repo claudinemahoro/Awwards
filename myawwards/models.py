1e2ef15cd447d88420af84cefc958ae087778752
claudinemahoro <claudinemah3@gmail.com>
2021-01-19 18:04:52 +0200

    creating comment model

--- myawwards/models.py
+++ myawwards/models.py
@@ -48,9 +48,9 @@ class Project(models.Model):
         images=cls.objects.all().prefetch_related('comment_set')
         return images
 
-# class Comment(models.Model):
-#     posted_by=models.ForeignKey(User, on_delete=models.CASCADE,null=True)
-#     comment_image=models.ForeignKey(Project,on_delete=models.CASCADE,null=True)
-#     comment=models.CharField(max_length=20,null=True)
-#     def __str__(self):
-#         return self.posted_by
+class Comment(models.Model):
+    posted_by=models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    comment_image=models.ForeignKey(Project,on_delete=models.CASCADE,null=True)
+    comment=models.CharField(max_length=20,null=True)
+    def __str__(self):
+        return self.posted_by

6994ae60178e7e777efb3baa1139697baf818a03
claudinemahoro <claudinemah3@gmail.com>
2021-01-21 13:24:03 +0200

    testing instance for profile model

--- myawwards/tests.py
+++ myawwards/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import *
+from django.contrib.auth.models import User
+
+# Create your tests here.
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.coco = Profile( profile_picture  = '/', bio = 'my tests', contact='mahorotesting@gmail.com')
+
+# Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.coco,Profile))
+
+  

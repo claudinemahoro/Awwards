e4d56bc281b25ba9b331b23f43a06ade38d86a0f
claudinemahoro <claudinemah3@gmail.com>
2021-01-21 03:36:16 +0200

    creating the edit profile view

--- myawwards/views.py
+++ myawwards/views.py
@@ -41,3 +41,17 @@ def profile(request, username=None):
 
 	return render(request,"profile.html",{"pi_images":pi_images})
 
+@login_required(login_url='/accounts/login/')
+def profile_edit(request):
+    current_user=request.user
+    if request.method=='POST':
+        form=ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            image=form.save(commit=False)
+            image.user=current_user
+            image.save()
+        return redirect('profile')
+    else:
+        form=ProfileForm()
+    return render(request,'profile_edit.html',{"form":form})
+
